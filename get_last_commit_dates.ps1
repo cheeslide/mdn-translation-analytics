23dd18880b276614245487192dd9f42be64888e3
cheeslide <175485195+cheeslide@users.noreply.github.com>
2025-01-27 13:43:50 +0800

    Delete get_last_commit_dates.ps1

--- get_last_commit_dates.ps1
+++
@@ -1,6 +0,0 @@
-$logFile = "md_logs.txt"
-$outFile = "old_files.csv"
-$targetFiles = "current_files.txt"
-git log --format=%ad --date=short --name-only | Out-File -FilePath $logFile -Encoding ascii
-git ls-files *.md | Out-File -FilePath $targetFiles -Encoding ascii
-python old.py $logFile $outFile $targetFiles
