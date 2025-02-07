bf305c5e334c8a3f50830599323192e5d7c32e78
cheeslide <175485195+cheeslide@users.noreply.github.com>
2025-02-07 17:57:53 +0800

    Update integrate.py

--- integrate.py
+++ integrate.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+OUTPUT_FILE_NAME = 'output.xlsx'
+
 distances_df = pd.read_csv('distances.csv')
 last_commit_date_df = pd.read_csv('last_commit_date.csv')
 no_sourceCommit_df = pd.read_csv('no_sourceCommit.txt', header=None, names=['Data'])  # 每行一个数据，没有分隔符
@@ -10,10 +12,10 @@ last_commit_date_df = last_commit_date_df.sort_values(by='Last Modified Date')
 no_sourceCommit_df = no_sourceCommit_df.sort_values(by='Data')
 untranslated_df = untranslated_df.sort_values(by='Data')
 
-with pd.ExcelWriter('output.xlsx') as writer:
+with pd.ExcelWriter(OUTPUT_FILE_NAME) as writer:
     distances_df.to_excel(writer, sheet_name='Distances', index=False)
     last_commit_date_df.to_excel(writer, sheet_name='Last_Commit_Date', index=False)
     no_sourceCommit_df.to_excel(writer, sheet_name='No_Source_Commit', index=False)
     untranslated_df.to_excel(writer, sheet_name='Untranslated', index=False)
 
-print("数据已成功写入 output.xlsx")
+print(f"writen to ${OUTPUT_FILE_NAME}")
