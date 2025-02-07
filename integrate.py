760e25716f00f7615d3a65d4a525f2adaae1446d
cheeslide <175485195+cheeslide@users.noreply.github.com>
2025-02-07 22:49:51 +0800

    Update integrate.py

--- integrate.py
+++ integrate.py
@@ -1,21 +1,33 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
 OUTPUT_FILE_NAME = 'output.xlsx'
 
+# 读取数据
 distances_df = pd.read_csv('distances.csv')
 last_commit_date_df = pd.read_csv('last_commit_date.csv')
 no_sourceCommit_df = pd.read_csv('no_sourceCommit.txt', header=None, names=['Data'])  # 每行一个数据，没有分隔符
 untranslated_df = pd.read_csv('untranslated.txt', header=None, names=['Data'])  # 每行一个数据，没有分隔符
 
+distances_df = distances_df[distances_df['Distance'] != 0]
+
+# 处理 Last Modified Date 列，删除在当前日期三个月之内的行
+current_date = datetime.now()
+three_months_ago = current_date - timedelta(days=90)
+three_months_ago_str = three_months_ago.strftime('%Y-%m-%d')
+last_commit_date_df = last_commit_date_df[last_commit_date_df['Last Modified Date'] < three_months_ago_str]
+
+# 排序数据
 distances_df = distances_df.sort_values(by='Distance', ascending=False)
 last_commit_date_df = last_commit_date_df.sort_values(by='Last Modified Date')
 no_sourceCommit_df = no_sourceCommit_df.sort_values(by='Data')
 untranslated_df = untranslated_df.sort_values(by='Data')
 
+# 写入 Excel 文件
 with pd.ExcelWriter(OUTPUT_FILE_NAME) as writer:
     distances_df.to_excel(writer, sheet_name='Distances', index=False)
     last_commit_date_df.to_excel(writer, sheet_name='Last_Commit_Date', index=False)
     no_sourceCommit_df.to_excel(writer, sheet_name='No_Source_Commit', index=False)
     untranslated_df.to_excel(writer, sheet_name='Untranslated', index=False)
 
-print(f"writen to ${OUTPUT_FILE_NAME}")
+print(f"written to {OUTPUT_FILE_NAME}")
